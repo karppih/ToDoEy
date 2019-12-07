08a68dfc5b49c664cd7a7c7ec507bfb46a15cc31
karppih <13274697+karppih@users.noreply.github.com>
2019-12-07 15:59:35 -0500

    Installed RealmSwift CocoaPod

---
+++ ToDoEy/Data Model/Data.swift
@@ -0,0 +1,14 @@
+//
+//  Data.swift
+//  ToDoEy
+//
+//  Created by Helen Karppi on 12/7/19.
+//  Copyright © 2019 Helen Karppi. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Data: Object {
+    
+}
