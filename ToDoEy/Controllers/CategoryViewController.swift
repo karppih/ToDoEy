396561575c69d507f21b99211890d7ddbed52c88
karppih <13274697+karppih@users.noreply.github.com>
2019-12-07 08:34:45 -0500

    Category view controller challenge completed.

--- ToDoEy/Controllers/CategoryViewController.swift
+++ ToDoEy/Controllers/CategoryViewController.swift
@@ -12,27 +12,110 @@ import CoreData
 //Goal: list of categories that displays and
 
 class CategoryViewController: UITableViewController {
-
+    
+    var categoryArray = [Category]()
+    
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        print(dataFilePath)
+        
+//        let titleList = ["Shopping", "Cleaning"]
+//        for newTitle in titleList {
+//            var newCat = Category(context: context)
+//            newCat.name = newTitle
+//            categoryArray.append(newCat)
+//        }
+        print("\(categoryArray.count) categories.")
+        
+        loadCategories()
+        loadTable()
+        print("\(categoryArray.count) categories.")
 
-
+        
+    }
+    
+    func loadTable() {
+        DispatchQueue.main.async {
+            self.tableView.reloadData()
+        }
     }
     
     //MARK: - TableView Data Source methods
-//    So we can display categories
-
- 
-//MARK: - Add new categories
-    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+    //    So we can display categories
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
+        let category = categoryArray[indexPath.row]
+        
+        cell.textLabel?.text = category.name
+        
+        return cell
     }
     
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categoryArray.count
+    }
+    
+    
+    //MARK: - Add new categories
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        
+        var category = Category(context: context)
+        
+        //Create alert popup
+        let alert = UIAlertController(title: "Add new category", message: "", preferredStyle: .alert)
+        
+        
+        let action = UIAlertAction(title: "Add", style: .default) { (action) in
+            category.name = textField.text!
+            self.categoryArray.append(category)
+            
+            self.saveCategories()
+        }
+        
+        //Add text field to alert popup
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new category"
+            //            let item = Item(context: self.context)
+            category.name = alertTextField.text!
+            textField = alertTextField
+            print("Add text field")
+            print(category.name)
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+
     
     //MARK: - Data Manipulation
     //Save and load data
     
+    func saveCategories() {
+        do {
+            try context.save()
+        } catch {
+            print("Error saving context \(error)")
+        }
+        
+        self.tableView.reloadData()
+    }
+    
+    func loadCategories(with request: NSFetchRequest<Category> = Category.fetchRequest()) {
+        do {
+            categoryArray = try context.fetch(request)
+        } catch {
+            print("Error fetching data from context \(error)")
+        }
+        
+        tableView.reloadData()
+    }
+    
     //MARK: - TableView Delgate Methods
     
-
+    
     
 }
